d3fd5ac829959464e3d4a56478316285232f59cf
Rokon-556 <rokonrahman556@gmail.com>
2023-08-20 22:20:44 +0600

    Recommended Product Details page dynamic done

--- lib/pages/food/recommeded_food_details.dart
+++ lib/pages/food/recommeded_food_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/data/controller/popular_product_list_controller.dart';
 import 'package:food_delivery/data/controller/recommended_product_list_controller.dart';
 import 'package:food_delivery/routes/app_routes.dart';
 import 'package:food_delivery/utils/app_constants.dart';
@@ -10,105 +11,167 @@ import 'package:food_delivery/widgets/bottom_section.dart';
 import 'package:food_delivery/widgets/expandable_text_widget.dart';
 import 'package:get/get.dart';
 
+import '../../data/controller/cart_controller.dart';
+
 class RecommendedFoodDetails extends StatelessWidget {
   final int pageId;
-  const RecommendedFoodDetails({super.key,required this.pageId});
+  const RecommendedFoodDetails({super.key, required this.pageId});
 
   @override
   Widget build(BuildContext context) {
-    var product = Get.find<RecommenededProductListController>().recommendedProductList[pageId];
+    var product = Get.find<RecommenededProductListController>()
+        .recommendedProductList[pageId];
+    Get.find<PopularProductListController>()
+        .initProduct(product, Get.find<CartController>());
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            toolbarHeight: 70,
-            backgroundColor: AppColors.yellowColor,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: ()=>Get.toNamed(AppRoutes.getInitial()),
-                  child: const AppIcon(icon: Icons.clear),
-                ),
-                const AppIcon(icon: Icons.shopping_cart)
-              ],
-            ),
-            expandedHeight: 300,
-            pinned: true,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(20),
-              child: Container(
-                width: double.maxFinite,
-                padding: const EdgeInsets.only(top: 5, bottom: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(Dimension.radius20),
-                      topRight: Radius.circular(Dimension.radius20),
-                    )),
-                child: Center(
-                  child: BigText(
-                    text: product.name ?? '',
-                    textSize: Dimension.font20 + 6,
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              toolbarHeight: 70,
+              backgroundColor: AppColors.yellowColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () => Get.toNamed(AppRoutes.getInitial()),
+                    child: const AppIcon(icon: Icons.clear),
+                  ),
+                  // const AppIcon(icon: Icons.shopping_cart)
+                  GetBuilder<PopularProductListController>(
+                      builder: (popularController) {
+                        return Stack(
+                          children: [
+                            AppIcon(icon: Icons.shopping_cart),
+                            popularController.totalItems >= 1
+                                ? Positioned(
+                              top: 0,
+                              right: 0,
+                              child: AppIcon(
+                                icon: Icons.circle,
+                                size: 20,
+                                iconColor: Colors.transparent,
+                                bgColor: AppColors.mainColor,
+                              ),
+                            )
+                                : Container(),
+                            popularController.totalItems >= 1
+                                ? Positioned(
+                                top: 3,
+                                right: 3,
+                                child: BigText(
+                                  text:
+                                  popularController.totalItems.toString(),
+                                  textSize: 12,
+                                  color: Colors.white,
+                                ))
+                                : Container(),
+                          ],
+                        );
+                      })
+                ],
+              ),
+              expandedHeight: 300,
+              pinned: true,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(20),
+                child: Container(
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.only(top: 5, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Dimension.radius20),
+                        topRight: Radius.circular(Dimension.radius20),
+                      )),
+                  child: Center(
+                    child: BigText(
+                      text: product.name ?? '',
+                      textSize: Dimension.font20 + 6,
+                    ),
                   ),
                 ),
               ),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(
-                AppConstants.BASE_URL+AppConstants.UPLOAD+product.img,
-                width: double.maxFinite,
-                fit: BoxFit.cover,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  AppConstants.BASE_URL + AppConstants.UPLOAD + product.img,
+                  width: double.maxFinite,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: Dimension.width20),
+                    child: ExpandableTextWidget(
+                      text: product.description ?? '',
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: GetBuilder<PopularProductListController>(
+          builder: (popularProductController) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: Dimension.width20),
-                  child:  ExpandableTextWidget(
-                    text:
-                        product.description ?? '',
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Dimension.width20 * 2.5,
+                    vertical: Dimension.height10,
                   ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: Dimension.width20 * 2.5,
-              vertical: Dimension.height10,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AppIcon(
-                  icon: Icons.remove,
-                  bgColor: AppColors.mainColor,
-                  iconSize: Dimension.icon24,
-                  iconColor: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          popularProductController.setQuantity(false);
+                        },
+                        child: AppIcon(
+                          icon: Icons.remove,
+                          bgColor: AppColors.mainColor,
+                          iconSize: Dimension.icon24,
+                          iconColor: Colors.white,
+                        ),
+                      ),
+                      BigText(
+                        text:
+                            '\$${product.price.toString()} X ${popularProductController.inCartItem} ',
+                        textSize: Dimension.font20 + 6,
+                        color: AppColors.mainBlackColor,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          popularProductController.setQuantity(true);
+                        },
+                        child: AppIcon(
+                          icon: Icons.add,
+                          bgColor: AppColors.mainColor,
+                          iconSize: Dimension.icon24,
+                          iconColor: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                BigText(text: '\$${product.price.toString()} X 0 ',textSize: Dimension.font20+6,color: AppColors.mainBlackColor,),
-                AppIcon(
-                  icon: Icons.add,
-                  bgColor: AppColors.mainColor,
-                  iconSize: Dimension.icon24,
-                  iconColor: Colors.white,
+                BottomSection(
+                  widget: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor,
+                  ),
+                  price: product.price.toString(),
+                  onTap: (){
+                    popularProductController.addItem(product);
+                  },
                 )
               ],
-            ),
-          ),
-          BottomSection(widget: Icon(Icons.favorite,color: AppColors.mainColor,),price: product.price.toString(),)
-        ],
-      ),
-    );
+            );
+          },
+        ));
   }
 }
